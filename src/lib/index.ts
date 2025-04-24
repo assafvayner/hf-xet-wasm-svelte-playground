84299fa338f3d8b9c7ca088d98be73eb57fc3e17
Assaf Vayner <assaf@huggingface.co>
2025-04-24 14:56:03 -0700

    update

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,41 +1,13 @@
-import init, { chunk_vec, chunk_array_buffer, ChunkInfo } from 'hf_xet_wasm';
+import { chunk, ChunkInfo } from '../../../../hf/xet-core/hf_xet_wasm/pkg/hf_xet_wasm';
 
 const DEFAULT_SIZE_IN_MB = 20;
 
 export async function callMe() {
-	await init();
-
 	const buffer = generateRandomData(DEFAULT_SIZE_IN_MB);
-	const result1: ChunkInfo[] = chunk_array_buffer(buffer);
-	const array = new Uint8Array(buffer);
-	const result2: ChunkInfo[] = chunk_vec(array);
-	console.log(`chunk array buffer:\n${result1}`);
-	console.log(`chunk vec:\n${result2}`);
-
-	if (result1.length !== result2.length) {
-		console.error('mismatch in length');
-		return;
-	}
+	const result1: ChunkInfo[] = chunk(buffer);
+	console.log(`chunk vec:\n${result1}`);
 
 	console.log(`num chunks returned ${result1.length}`);
-
-	let err;
-	for (let i = 0; i < result1.length; i++) {
-		const chunk_info_1 = result1[i];
-		const chunk_info_2 = result2[i];
-		if (chunk_info_1.hash !== chunk_info_2.hash) {
-			console.error(`index ${i}, hash mismatch 1: ${chunk_info_1.hash} 2: ${chunk_info_2.hash}`);
-			err = true;
-		}
-
-		if (chunk_info_1.len !== chunk_info_2.len) {
-			console.error(`index ${i}, len mismatch 1: ${chunk_info_1.len} 2: ${chunk_info_2.len}`);
-			err = true;
-		}
-	}
-	if (err) {
-		return;
-	}
 
 	for (let i = 0; i < result1.length; i++) {
 		const chunk_info_1 = result1[i];
@@ -49,7 +21,7 @@ export async function callMe() {
  * @param sizeInMB The size of the data to generate in megabytes.
  * @returns An ArrayBuffer containing the random data, or undefined on error.
  */
-function generateRandomData(sizeInMB: number): ArrayBuffer {
+function generateRandomData(sizeInMB: number): Uint8Array {
 	// Calculate the size in bytes.
 	const sizeInBytes = sizeInMB * 1024 * 1024;
 
@@ -69,5 +41,5 @@ function generateRandomData(sizeInMB: number): ArrayBuffer {
 	for (let i = 0; i < sizeInBytes; i++) {
 		view[i] = Math.floor(Math.random() * 256); // 0-255
 	}
-	return buffer;
+	return view;
 }
